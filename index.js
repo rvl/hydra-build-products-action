d463d8fd19b82fc4ef1169df4d431f06dc5e3c26
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2020-11-23 15:18:56 +0800

    Add retrying of pending eval

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// This script downloads builds of cardano-wallet from Hydra.
-// It is meant for use within the travis release script.
+// This github actions workflow queries evaluation and build product
+// urls from Hydra.
+// Adapted from cardano-wallet/scripts/travis-download-builds.js
 
 const fs = require('fs');
 const path = require('path');
@@ -51,19 +52,23 @@ function findCardanoWalletEval(api, rev) {
   return findEvalByCommit(apiapi, "Cardano", "cardano-wallet", rev);
 }
 
-async function findEvalsFromGitHub(hydra, github, owner, repo, ref, page) {
+async function findEvalsFromGitHub(hydra, github, owner, repo, rev, page) {
   const q = "?per_page=100" + (page ? `&page=${page}` : "");
-  const r = await github.get(`repos/${owner}/${repo}/commits/${ref}/statuses${q}`);
+  const r = await github.get(`repos/${owner}/${repo}/commits/${rev}/statuses${q}`);
 
   if (_.isEmpty(r.data)) {
-    console.log(`No more pages from GitHub.`);
-    return [];
+    console.log(`Eval not found, and no more pages from GitHub.`);
+    console.log(`Waiting for updated CI status.`);
+    await sleep(5000);
+    return await findEvalsFromGitHub(hydra, github, owner, repo, rev);
   }
 
   const statuses = _.filter(r.data, status => status.context.startsWith("ci/hydra-eval"));
   const successful = _.filter(statuses, { state: "success" });
   const pending = _.filter(statuses, { state: "pending" });
   const failed = _.difference(statuses, successful, pending);
+
+  console.log("statuses", JSON.stringify(statuses));
 
   console.log(`Found ${statuses.length} eval statuses:  successful=${successful.length}  pending=${pending.length}  failed=${failed.length}`);
 
@@ -77,17 +82,45 @@ async function findEvalsFromGitHub(hydra, github, owner, repo, ref, page) {
 
   if (_.isEmpty(evals)) {
     if (pending.length) {
-       console.log("Eval is pending - trying again...");
-       await sleep(1000);
-       return await findEvalsFromGitHub(hydra, github, owner, repo, ref);
+      console.log("Eval is pending - trying again...");
+      return waitForPendingEval(hydra, repo, rev, pending);
     } else if (failed.length) {
       console.error("Can't get eval - it was not successful.");
       return null;
     } else {
       const next = (page || 1) + 1;
       console.log(`Eval not found - trying page ${next}`);
-      return await findEvalsFromGitHub(hydra, github, owner, repo, ref, next);
+      return await findEvalsFromGitHub(hydra, github, owner, repo, rev, next);
     }
+  } else {
+    return evals;
+  }
+}
+
+async function waitForPendingEval(hydra, srcName, rev, pendings) {
+  await sleep(10000);
+
+  let evals = [];
+  for await (const pending of pendings) {
+    const jobset = await hydra.get(pending.target_url);
+    if (jobset.data.errormsg) {
+      console.log(`There is a currently an evaluation error for jobset: ${pending.target_url}`);
+    }
+
+    const evalsURL = pending.target_url.replace(/#.*$/, "/evals");
+    const evals = await hydra.get(evalsURL);
+    console.log(JSON.stringify(evals.data));
+    console.log(`There are ${evals.data.evals.length} eval(s)`);
+    const eval = _.find(evals.data.evals, e => e.jobsetevalinputs[srcName] && e.jobsetevalinputs[srcName].revision === rev);
+    if (eval) {
+      console.log("Found eval", eval);
+      evals.push(eval);
+    }
+  }
+
+  if (_.isEmpty(evals)) {
+    console.log("Eval is still pending - trying again...");
+    return waitForPendingEval(hydra, srcName, rev, pendings);
   } else {
     return evals;
   }
@@ -147,20 +180,25 @@ function sleep(ms = 0) {
 };
 
 async function main() {
-  const hydraURL = core.getInput('hydra');
-  const jobs = core.getInput('jobs').split(/ /);
+  const hydraURL = process.env.HYDRA_URL || core.getInput('hydra');
+  const jobs = process.env.HYDRA_JOBS || core.getInput('jobs').split(/ /);
+  console.log("INPUT hydraURL:", hydraURL);
+  console.log("INPUT jobs:", jobs);
 
   const payload = github.context.payload;
-  console.log("payload", payload);
+  console.log("github payload:", payload);
 
   const spec = {
-    owner: payload.repository.owner.login,
-    repo: payload.repository.name,
-    rev: payload.after,
+    owner: process.env.REPO_OWNER || payload.repository.owner.login,
+    repo: process.env.REPO_NAME || payload.repository.name,
+    rev: process.env.COMMIT || payload.after,
     jobs: _.map(jobs, name => { return { job: name, buildProducts: [1] }; })
   };
 
   const res = await download(hydraURL, spec);
+
+  console.log("OUTPUT eval:", res.eval);
+  console.log("OUTPUT builds:", res.builds);
 
   core.setOutput("eval", res.eval);
   core.setOutput("builds", res.builds.join(" "));
